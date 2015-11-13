718432dcecaedab3a0ff5b3992abdec3d414ae0a
Joaquin <juacocampero@gmail.com>
2015-11-13 15:46:33 -0300

    pfsa

--- api/models/User.js
+++ api/models/User.js
@@ -7,12 +7,13 @@ var _super = require('sails-permissions/api/models/User');
 _.merge(exports, _super);
 _.merge(exports, {
     // Extend with custom logic here by adding additional fields, methods, etc.
-    /*atributes: {
-        password: {
-            type: 'string',
-            required: true
+    atributes: {
+        getGravatarUrl: function () {
+            var md5 = crypto.createHash('md5');
+            md5.update(this.email || '');
+            return 'https://gravatar.com/avatar/'+ md5.digest('hex') + '?d=retro';
         }
-    },
+    }/*,
     beforeCreate: function (user, next) {
         bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(user.password, salt, function (err, hash) {
